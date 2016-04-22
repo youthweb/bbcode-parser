0a7b94c792305af35c163f27e34f9c0b6047bbac
Art4 <art4@wlabs.de>
2016-04-22 10:52:47 +0200

    Vorbereitungen für PHPUnit und Travis-CI

---
+++ tests/Bootstrap.php
@@ -0,0 +1,23 @@
+<?php
+
+
+spl_autoload_register(function ($class)
+{
+	$class = str_replace('\\', '/', $class);
+	$class = str_replace('Youthweb/BBCodeParser/Tests/', '', $class);
+
+	$path = dirname(__FILE__).'/'.$class.'.php';
+
+	if ( file_exists($path) )
+	{
+		require_once $path;
+	}
+});
+
+if ( ! @include_once __DIR__.'/../vendor/autoload.php')
+{
+	exit("You must set up the project dependencies, run the following commands:
+> wget http://getcomposer.org/composer.phar
+> php composer.phar install
+");
+}
