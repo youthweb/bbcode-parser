eaef4f4fc3209fe2ab980ca530f26f39f1882563
Art4 <art4@wlabs.de>
2016-04-22 15:51:35 +0200

    Tests für ListDefinition

---
+++ tests/unit/Definition/ListDefinitionTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Youthweb\BBCodeParser\Tests\Unit\Definition;
+
+use JBBCode\ElementNode;
+use JBBCode\TextNode;
+use Youthweb\BBCodeParser\Definition\ListDefinition;
+
+class ListDefinitionTest extends \PHPUnit_Framework_TestCase
+{
+	/**
+	 * @test
+	 */
+	public function testAsHtml()
+	{
+		$text     = '[*]Hello World!
+[*]foobar';
+		$expected = '<!-- no_p --><ul type="disc">'.PHP_EOL.
+"\t".'<li>Hello World!</li>'.PHP_EOL.
+"\t".'<li>foobar</li>'.PHP_EOL.
+'</ul>'.PHP_EOL.
+'<!-- no_p -->';
+
+		$child = $this->getMockBuilder('JBBCode\TextNode')
+			->setConstructorArgs([$text])
+			->setMethods(null)
+			->getMock();
+
+		$elementNode = $this->getMockBuilder('JBBCode\ElementNode')
+			->setMethods(['getAttribute', 'getChildren'])
+			->getMock();
+
+		$elementNode->method('getAttribute')
+			->willReturn(null);
+
+		$elementNode->method('getChildren')
+			->willReturn([$child]);
+
+		$listDefinition = new ListDefinition();
+
+		$this->assertSame($expected, $listDefinition->asHtml($elementNode));
+	}
+}
