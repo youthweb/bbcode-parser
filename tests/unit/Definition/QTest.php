ce2297316777eace3095ab2f16a4098cae64f9e7
Art4 <art4@wlabs.de>
2016-04-23 20:51:21 +0200

    Tests für Q Definition

---
+++ tests/unit/Definition/QTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Youthweb\BBCodeParser\Tests\Unit\Definition;
+
+use JBBCode\ElementNode;
+use JBBCode\TextNode;
+use Youthweb\BBCodeParser\Definition\Q;
+use Youthweb\BBCodeParser\Tests\Fixtures\MockerTrait;
+
+class QTest extends \PHPUnit_Framework_TestCase
+{
+	use MockerTrait;
+
+	/**
+	 * @dataProvider dataProvider
+	 */
+	public function testAsHtml($text, $attribute, $expected)
+	{
+		$elementNode = $this->buildElementNodeMock($text, $attribute);
+
+		$listDefinition = new Q();
+
+		$this->assertSame($expected, $listDefinition->asHtml($elementNode));
+	}
+
+	/**
+	 * data provider
+	 */
+	public function dataProvider()
+	{
+		return [
+			[
+				'quote content',
+				null,
+				'<blockquote title="Zitat"><cite></cite>quote content</blockquote>',
+			],
+			[
+				'quote content',
+				'someone',
+				'<blockquote title="Zitat"><cite>someone</cite>quote content</blockquote>',
+			],
+		];
+	}
+}
