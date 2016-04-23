5af10c204a915c62efcc349421c1dbecc212ce18
Art4 <art4@wlabs.de>
2016-04-23 21:07:10 +0200

    Tests für Z und ZOption

---
+++ tests/unit/Definition/ZOptionTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Youthweb\BBCodeParser\Tests\Unit\Definition;
+
+use JBBCode\ElementNode;
+use JBBCode\TextNode;
+use Youthweb\BBCodeParser\Definition\ZOption;
+use Youthweb\BBCodeParser\Tests\Fixtures\MockerTrait;
+
+class ZOptionTest extends \PHPUnit_Framework_TestCase
+{
+	use MockerTrait;
+
+	/**
+	 * @dataProvider dataProvider
+	 */
+	public function testAsHtml($text, $attribute, $expected)
+	{
+		$elementNode = $this->buildElementNodeMock($text, $attribute);
+
+		$listDefinition = new ZOption();
+
+		$this->assertSame($expected, $listDefinition->asHtml($elementNode));
+	}
+
+	/**
+	 * data provider
+	 */
+	public function dataProvider()
+	{
+		return [
+			[
+				'quote content',
+				'someone',
+				'<blockquote title="Zitat"><cite>someone</cite>quote content</blockquote>',
+			],
+			[
+				'quote content',
+				['someone'],
+				'<blockquote title="Zitat"><cite>someone</cite>quote content</blockquote>',
+			],
+			[
+				'<blockquote title="Zitat"><cite>first</cite>content 1</blockquote>content 2',
+				'second',
+				'<blockquote title="Zitat"><blockquote title="Zitat"><cite>first</cite>content 1</blockquote><cite>second</cite>content 2</blockquote>',
+			],
+		];
+	}
+}
---
+++ tests/unit/Definition/ZTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Youthweb\BBCodeParser\Tests\Unit\Definition;
+
+use JBBCode\ElementNode;
+use JBBCode\TextNode;
+use Youthweb\BBCodeParser\Definition\Z;
+use Youthweb\BBCodeParser\Tests\Fixtures\MockerTrait;
+
+class ZTest extends \PHPUnit_Framework_TestCase
+{
+	use MockerTrait;
+
+	/**
+	 * @dataProvider dataProvider
+	 */
+	public function testAsHtml($text, $attribute, $expected)
+	{
+		$elementNode = $this->buildElementNodeMock($text, $attribute);
+
+		$listDefinition = new Z();
+
+		$this->assertSame($expected, $listDefinition->asHtml($elementNode));
+	}
+
+	/**
+	 * data provider
+	 */
+	public function dataProvider()
+	{
+		return [
+			[
+				'quote content',
+				null,
+				'<blockquote title="Zitat"><cite></cite>quote content</blockquote>',
+			],
+			[
+				'quote content',
+				null,
+				'<blockquote title="Zitat"><cite></cite>quote content</blockquote>',
+			],
+			[
+				'<blockquote title="Zitat"><cite>first</cite>content 1</blockquote>content 2',
+				null,
+				'<blockquote title="Zitat"><blockquote title="Zitat"><cite>first</cite>content 1</blockquote><cite></cite>content 2</blockquote>',
+			],
+		];
+	}
+}
