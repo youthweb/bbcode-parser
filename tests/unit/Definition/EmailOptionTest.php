343be6ed56282e5be8429d2d87a84e8ab5459abf
Art4 <art4@wlabs.de>
2016-04-23 21:45:28 +0200

    Tests für EmailOption Definition

---
+++ tests/unit/Definition/EmailOptionTest.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace Youthweb\BBCodeParser\Tests\Unit\Definition;
+
+use JBBCode\ElementNode;
+use JBBCode\TextNode;
+use Youthweb\BBCodeParser\Definition\EmailOption;
+use Youthweb\BBCodeParser\Tests\Fixtures\MockerTrait;
+
+class EmailOptionTest extends \PHPUnit_Framework_TestCase
+{
+	use MockerTrait;
+
+	/**
+	 * @dataProvider dataProvider
+	 */
+	public function testAsHtml($text, $attribute, $expected)
+	{
+		$elementNode = $this->buildElementNodeMock($text, $attribute);
+
+		$config = $this->getMockBuilder('Youthweb\BBCodeParser\Config')
+			->setMethods(['get'])
+			->getMock();
+
+		$config->method('get')
+			->with('callbacks.email_content.protect_email')
+			->willReturn(false);
+
+		$definition = new EmailOption($config);
+
+		$this->assertSame($expected, $definition->asHtml($elementNode));
+	}
+
+	/**
+	 * @dataProvider dataProviderProtectedEmail
+	 */
+	public function testAsHtmlWithProtectedEmail($text, $attribute, $expected)
+	{
+		$elementNode = $this->buildElementNodeMock($text, $attribute);
+
+		$config = $this->getMockBuilder('Youthweb\BBCodeParser\Config')
+			->setMethods(['get'])
+			->getMock();
+
+		$config->method('get')
+			->with('callbacks.email_content.protect_email')
+			->willReturn(true);
+
+		$definition = new EmailOption($config);
+
+		$this->assertSame($expected, $definition->asHtml($elementNode));
+	}
+
+	/**
+	 * data provider
+	 */
+	public function dataProvider()
+	{
+		return [
+			[
+				'email',
+				'mail@example.org',
+				'<a href="mailto:mail@example.org">email</a>',
+			],
+			[
+				'email',
+				['mail@example.org'],
+				'<a href="mailto:mail@example.org">email</a>',
+			],
+			[
+				'text',
+				'invalid email',
+				'text',
+			],
+			[
+				'',
+				'mail@example.org',
+				'<a href="mailto:mail@example.org">mail@example.org</a>',
+			],
+		];
+	}
+
+	/**
+	 * data provider for protected emails
+	 */
+	public function dataProviderProtectedEmail()
+	{
+		return [
+			[
+				'email',
+				'mail@example.org',
+				'<script type="text/javascript">(function() {var user = "mail";var at = "@";var server = "example.org";document.write(\'<a href="\' + \'mail\' + \'to:\' + user + at + server + \'">email</a>\');})();</script>',
+			],
+			[
+				'email',
+				['mail@example.org'],
+				'<script type="text/javascript">(function() {var user = "mail";var at = "@";var server = "example.org";document.write(\'<a href="\' + \'mail\' + \'to:\' + user + at + server + \'">email</a>\');})();</script>',
+			],
+			[
+				'text',
+				'invalid email',
+				'text',
+			],
+			[
+				'',
+				'mail@example.org',
+				'<script type="text/javascript">(function() {var user = "mail";var at = "@";var server = "example.org";document.write(\'<a href="\' + \'mail\' + \'to:\' + user + at + server + \'">mail[at]example.org</a>\');})();</script>',
+			],
+		];
+	}
+}
