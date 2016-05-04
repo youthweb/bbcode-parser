89b124af2ff8006bdab5c46a4fce84de46525274
Art4 <art4@wlabs.de>
2016-05-04 08:19:13 +0200

    Tests für Headline parsing

--- tests/integration/ParsingTest.php
+++ tests/integration/ParsingTest.php
@@ -44,4 +44,57 @@ class ParsingTest extends \PHPUnit_Framework_TestCase
 
 		$this->assertSame($parser->parse($text), $expected);
 	}
+
+	/**
+	 * @dataProvider HeaderDataProvider
+	 */
+	public function testParseHeaderCode($text, $expected)
+	{
+		$config = [
+			'parse_headlines' => true,
+		];
+
+		$parser = new Manager();
+
+		$this->assertSame($expected, $parser->parse($text, $config));
+	}
+
+	/**
+	 * @HeaderDataProvider
+	 */
+	public function HeaderDataProvider()
+	{
+		return [
+			[
+				'[h1]Header 1[/h1]',
+				'<h1>Header 1</h1>',
+			],
+			[
+				'[h2]Header 2[/h2]',
+				'<h2>Header 2</h2>',
+			],
+			[
+				'[h3]Header 3[/h3]',
+				'<h3>Header 3</h3>',
+			],
+			[
+				'[h4]Header 4[/h4]',
+				'<h4>Header 4</h4>',
+			],
+			[
+				'[h5]Header 5[/h5]',
+				'<h5>Header 5</h5>',
+			],
+			[
+				'[h6]Header 6[/h6]',
+				'<h6>Header 6</h6>',
+			],
+			[
+				'[h7]Header 7[/h7]',
+				'<p>[h7]Header 7[/h7]</p>',
+			],
+		];
+	}
+
+
 }
