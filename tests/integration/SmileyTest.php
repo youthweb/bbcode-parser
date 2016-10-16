3c9e5b622a44653b4ec27cd9e27b1986b08a2c38
Art4 <art4@wlabs.de>
2016-10-16 14:24:30 +0200

    Spaces to taps

--- tests/integration/SmileyTest.php
+++ tests/integration/SmileyTest.php
@@ -8,23 +8,23 @@ use Youthweb\BBCodeParser\Visitor\VisitorSmiley;
 class SmileyTest extends \PHPUnit_Framework_TestCase
 {
 
-    /**
-     * @test
-     */
-    public function testParseSmileyCode()
-    {
-        $text     = 'My mistake :-[';
-        $expected = '<p>My mistake <img src="https://youthweb.net/vendor/smilies/49_2.gif" alt=":-[" title=":-[" /></p>';
+	/**
+	 * @test
+	 */
+	public function testParseSmileyCode()
+	{
+		$text     = 'My mistake :-[';
+		$expected = '<p>My mistake <img src="https://youthweb.net/vendor/smilies/49_2.gif" alt=":-[" title=":-[" /></p>';
 
-        $parser = new Manager();
+		$parser = new Manager();
 
-        $config = [
-            'visitor' => [
-                'smiley' => new VisitorSmiley()
-            ]
-        ];
+		$config = [
+			'visitor' => [
+				'smiley' => new VisitorSmiley()
+			]
+		];
 
-        $this->assertSame($expected, $parser->parse($text, $config));
-    }
+		$this->assertSame($expected, $parser->parse($text, $config));
+	}
 
 }
