f4f255903c440b09595132d03a95d0d2621494cf
Art4 <art4@wlabs.de>
2021-11-23 15:07:24 +0100

    add unit tests for new code definitions

---
+++ tests/Unit/Definition/BoldTest.php
@@ -0,0 +1,60 @@
+<?php
+/*
+ * A BBCode-to-HTML parser for youthweb.net
+ * Copyright (C) 2016-2019  Youthweb e.V. <info@youthweb.net>
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Youthweb\BBCodeParser\Tests\Unit\Definition;
+
+use JBBCode\ElementNode;
+use JBBCode\TextNode;
+use Youthweb\BBCodeParser\Definition\Bold;
+use Youthweb\BBCodeParser\Tests\Fixtures\MockerTrait;
+
+class BoldTest extends \PHPUnit\Framework\TestCase
+{
+    use MockerTrait;
+
+    /**
+     * @dataProvider dataProvider
+     *
+     * @param mixed $text
+     * @param mixed $attribute
+     * @param mixed $expected
+     */
+    public function testAsHtml($text, $attribute, $expected)
+    {
+        $elementNode = $this->buildElementNodeMock($text, $attribute);
+
+        $definition = new Bold();
+
+        $this->assertSame($expected, $definition->asHtml($elementNode));
+    }
+
+    /**
+     * data provider
+     */
+    public function dataProvider()
+    {
+        return [
+            [
+                'This text should be bold.',
+                null,
+                '<b>This text should be bold.</b>',
+            ],
+        ];
+    }
+}
