a4c43722ccd5ad40b4efe3093f71589e7562e30a
Art4 <art4@wlabs.de>
2021-11-24 20:46:59 +0100

    Introduce new parser

---
+++ src/Parser/CommonMarkParser.php
@@ -0,0 +1,50 @@
+<?php
+/*
+ * A BBCode-to-HTML parser for youthweb.net
+ * Copyright (C) 2016-2021  Youthweb e.V. <info@youthweb.net>
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Youthweb\BBCodeParser\Parser;
+
+/**
+ * CommonMarkParser
+ */
+final class CommonMarkParser
+{
+    /**
+     * Create the Parser
+     */
+    public static function create(): CommonMarkParser
+    {
+        return new self();
+    }
+
+    /**
+     * Create the Parser
+     */
+    private function __construct()
+    {
+
+    }
+
+    /**
+     * parse $test with BBCode to Html
+     */
+    public function parseBbcodeToHtml(string $text): string
+    {
+        return $text;
+    }
+}
---
+++ tests/Integration/Parser/CommonMarkParserTest.php
@@ -0,0 +1,49 @@
+<?php
+/*
+ * A BBCode-to-HTML parser for youthweb.net
+ * Copyright (C) 2016-2021  Youthweb e.V. <info@youthweb.net>
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Youthweb\BBCodeParser\Tests\Integration\Parser;
+
+use PHPUnit\Framework\TestCase;
+use Youthweb\BBCodeParser\Parser\CommonMarkParser;
+
+class CommonMarkParserTest extends TestCase
+{
+    /**
+     * @dataProvider provideBbcodeAndHtml
+     *
+     * @param mixed $text
+     * @param mixed $expected
+     */
+    public function testParseBbcodeToHtml($text, $expected)
+    {
+        $parser = CommonMarkParser::create();
+
+        $this->assertSame($expected, $parser->parseBbcodeToHtml($text));
+    }
+
+    public function provideBbcodeAndHtml()
+    {
+        return [
+            [
+                'Hello World!',
+                'Hello World!',
+            ],
+        ];
+    }
+}
